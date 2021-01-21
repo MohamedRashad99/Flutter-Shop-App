120f813b78638cd7e23c4bf2dfbaeeab0d273d1f
Mohamed Rashad <mohamedrashad9797@gmail.com>
2021-01-21 15:27:18 +0200

    User Products Is Updated

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -31,34 +31,29 @@ class UserProductsScreen extends StatelessWidget {
         ],
       ),
       drawer: AppDrawer(),
-      body: FutureBuilder(
-        future: _refreshProducts(context),
-        builder: (ctx, AsyncSnapshot snapshot) =>
-        snapshot.connectionState == ConnectionState.waiting
-            ? Center(child: CircularProgressIndicator())
-            : RefreshIndicator(
-          onRefresh: () => _refreshProducts(context),
-          child: Consumer<Products>(
-            builder: (ctx, productsData, _) => Padding(
-              padding: EdgeInsets.all(8),
-              child: ListView.builder(
-                itemCount: productsData.items.length,
-                itemBuilder: (_, int index) => Column(
-                  children: [
-                    UserProductItem(
-                      productsData.items[index].id,
-                      productsData.items[index].title,
-                      productsData.items[index].imageUrl,
-                    ),
-                    Divider(),
-                  ],
-                ),
+      body: RefreshIndicator(
 
+        onRefresh: () => _refreshProducts(context),
+        child: Consumer<Products>(
+          builder: (ctx, productsData, _) => Padding(
+            padding: EdgeInsets.all(8),
+            child: ListView.builder(
+              itemCount: productsData.items.length,
+              itemBuilder: (_, int index) => Column(
+                children: [
+                  UserProductItem(
+                    productsData.items[index].id,
+                    productsData.items[index].title,
+                    productsData.items[index].imageUrl,
+                  ),
+                  Divider(),
+                ],
               ),
             ),
           ),
         ),
       ),
     );
+    //);
   }
 }
